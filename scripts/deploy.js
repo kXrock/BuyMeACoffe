b9ac9863f56a4e08854fc15b4c074b32acad3b3d
kXrock <keremkaya08@hotmail.com>
2023-03-20 00:45:18 +0300

    added ethers lab and fixed sol code

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,22 +6,13 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
+async function getbalance(address){
+  const balanceBigInt= await hre.waffle.provider.getbalance(address);
+  return hre.ethers.utils.formatEther(balanceBigInt)
+}
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  
 }
 
 // We recommend this pattern to be able to use async/await everywhere
